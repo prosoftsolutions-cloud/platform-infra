92cc2cd3d762462ca4f40bfca8fd8a85c9cc251f
sergeyprokofiev <sp@prosoftsolutions.cloud>
2025-11-28 15:10:05 +0100

    basic stack

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export function helloWorld(): string {
-  return "Hello, World!";
+import * as cdk from "aws-cdk-lib";
+import { PromoDeployStack } from "./stack";
+
+export function Deploy(): cdk.App {
+  const app = new cdk.App();
+
+  new PromoDeployStack(app, "PromoDeployStack", {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: "us-east-1",
+    },
+  });
+
+  return app;
 }
+
+// Run deployment when executed directly
+const app = Deploy();
+app.synth();
